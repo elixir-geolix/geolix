58bd7f22622ba366f28be882dd91147f084c6a72
Marc Neudert <marc.neudert@gmail.com>
2013-11-06 20:50:27 +0100

    prepares ip lookup

--- lib/geolix/database.ex
+++ lib/geolix/database.ex
@@ -1,4 +1,6 @@
 defmodule Geolix.Database do
+  use Bitwise, only_operators: true
+
   @metadata_marker <<171, 205, 239>> <> "MaxMind.com"
 
   @doc """
@@ -39,12 +41,52 @@ defmodule Geolix.Database do
   defp parse_stream(stream) do
     { metadata, _ } = drop_until_meta(stream) |> Geolix.Decoder.decode()
     metadata        = HashDict.new(metadata)
-    metadata        = HashDict.put(metadata, "node_byte_size", div(HashDict.get(metadata, "record_size", 0), 4))
+    metadata        = HashDict.put(metadata, "node_byte_size", div(HashDict.fetch!(metadata, "record_size"), 4))
     metadata        = HashDict.put(
       metadata, "search_tree_size",
-      HashDict.get(metadata, "node_count", 0) * HashDict.get(metadata, "node_byte_size", 0))
+      HashDict.fetch!(metadata, "node_count") * HashDict.fetch!(metadata, "node_byte_size"))
 
-    { metadata, {} }
+    #ip = {207,  97, 227, 245} # elixir-lang.org
+    ip = {108, 168, 255, 243} # maxmind.com
+
+    case parse_lookup_tree(ip, stream, metadata) do
+      0   -> IO.puts "Nothing found!"
+      ptr ->
+        ptr = ptr - HashDict.fetch!(metadata, "node_count") - 16
+        IO.puts "ptr: #{ptr} => " <> inspect(stream |> drop_until_data(metadata) |> Geolix.Decoder.decode(ptr))
+    end
+
+    { metadata, nil }
+  end
+
+  defp parse_lookup_tree(ip, stream, metadata) do
+    start_node = 96
+    #start_node = get_start_node(32, metadata, stream)
+
+    parse_lookup_tree_bitwise(ip, 0, 32, start_node, stream, metadata)
+  end
+
+  defp parse_lookup_tree_bitwise(ip, bit, bit_count, node, stream, metadata) when bit < bit_count do
+    if node >= HashDict.fetch!(metadata, "node_count") do
+      parse_lookup_tree_bitwise(nil, nil, nil, node, nil, metadata)
+    else
+      temp_bit = 0xFF &&& elem(ip, bit >>> 3)
+      node_bit = 1 &&& (temp_bit >>> 7 - rem(bit, 8))
+      node     = read_node(node, node_bit, stream, metadata)
+
+      parse_lookup_tree_bitwise(ip, bit + 1, bit_count, node, stream, metadata)
+    end
+  end
+  defp parse_lookup_tree_bitwise(_, _, _, node, _, metadata) do
+    node_count = HashDict.fetch!(metadata, "node_count")
+
+    cond do
+      node >  node_count -> node
+      node == node_count -> 0
+      true ->
+        IO.puts "Invalid node below node_count: #{node}"
+        0
+    end
   end
 
   defp drop_until_meta(stream) do
@@ -56,5 +98,53 @@ defmodule Geolix.Database do
     else
       Enum.drop(stream, 1) |> drop_until_meta()
     end
+  end
+
+  defp drop_until_data(stream, metadata) do
+    stream |> Enum.drop(HashDict.fetch!(metadata, "search_tree_size")) |> Enum.drop(16)
+  end
+
+  defp get_start_node(32, metadata, stream) do
+    case HashDict.fetch!(metadata, "ip_version") do
+      6 -> get_start_node_ipv4(0, 0, stream, metadata)
+      _ -> 0
+    end
+  end
+  defp get_start_node(_, _, _) do
+    0
+  end
+
+  defp get_start_node_ipv4(node, 96, _, _) do
+    node
+  end
+  defp get_start_node_ipv4(node, index, stream, metadata) do
+    read_node(node, 0, stream, metadata)
+      |> get_start_node_ipv4(index + 1, stream, metadata)
+  end
+
+  defp read_node(node, index, stream, metadata) do
+    IO.puts "Reading node: #{node} (index: #{index})"
+
+    offset = node * HashDict.fetch!(metadata, "node_byte_size")
+
+    case HashDict.fetch!(metadata, "record_size") do
+      24 ->
+        stream
+          |> Enum.drop(offset + index * 3)
+          |> Enum.take(3)
+          |> Enum.join()
+          |> decode_uint
+      invalid ->
+        IO.puts("Invalid record size in metadata: #{invalid}")
+        0
+    end
+  end
+
+  defp decode_uint(bytes) do
+    bitstring_to_list(bytes)
+      |> Enum.map(fn(x) -> integer_to_binary(x, 16) end)
+      |> Enum.join()
+      |> String.to_char_list!()
+      |> list_to_integer(16)
   end
 end
