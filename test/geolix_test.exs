7f3a140497a1b86d79fd993ce3462da19e0e6d5c
Marc Neudert <marc.neudert@gmail.com>
2014-05-30 19:34:44 +0200

    configures travis environment

--- test/geolix_test.exs
+++ test/geolix_test.exs
@@ -1,7 +1,22 @@
 defmodule GeolixTest do
   use ExUnit.Case, async: true
 
-  test "here be dragons!" do
-    assert(true)
+  test "set_db valid" do
+    Geolix.start_link()
+
+    assert :ok == Geolix.set_db_cities("/tmp")
+    assert :ok == Geolix.set_db_countries("/tmp")
+  end
+
+  test "set_db not a path" do
+    Geolix.start_link()
+
+    assert { :error, _ } = Geolix.set_db(:unused, "i-should-never-exist!")
+  end
+
+  test "set_db invalid type" do
+    Geolix.start_link()
+
+    assert { :error, _ } = Geolix.set_db(:invalid, ".")
   end
 end
