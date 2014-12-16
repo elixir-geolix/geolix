961532ddf4287c84a57ea9ea69414ccc6a6533ee
Marc Neudert <marc.neudert@gmail.com>
2014-12-16 21:07:48 +0100

    removes fixture file while testing missing metadata

--- test/geolix/database_test.exs
+++ test/geolix/database_test.exs
@@ -4,12 +4,8 @@ defmodule Geolix.DatabaseTest do
   alias Geolix.Database
 
   test "error if database contains no metadata" do
-    { fixture, filename, _ } =
-         Mix.Tasks.Geolix.Fixtures.list()
-      |> Enum.find(fn ({ fixture, _, _ }) -> :fixture_broken_pointers == fixture end)
+    path = Path.join([ __DIR__, "../fixtures/.gitignore" ]) |> Path.expand()
 
-    path = Path.join([ __DIR__, "../fixtures", filename ]) |> Path.expand()
-
-    assert { :error, :no_metadata } == Database.read_database(fixture, path)
+    assert { :error, :no_metadata } == Database.read_database(:invalid, path)
   end
 end
