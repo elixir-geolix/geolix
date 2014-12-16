63bf7694bf59a7c7ca57c26bf43fb2865a69e304
Marc Neudert <marc.neudert@gmail.com>
2014-12-16 21:10:31 +0100

    [resolves #1] uses :binary.match for database reading

--- lib/geolix/reader.ex
+++ lib/geolix/reader.ex
@@ -3,46 +3,19 @@ defmodule Geolix.Reader do
   Module to read database files and split them into data and metadata.
   """
 
-  @metadata_marker <<171, 205, 239>> <> "MaxMind.com"
+  @metadata_marker << 0xAB, 0xCD, 0xEF >> <> "MaxMind.com"
 
   @doc """
   Reads a database file and returns the data and metadata parts from it.
   """
   @spec read_database(String.t) :: { binary, binary }
   def read_database(filename) do
-    { :ok, %{ size: filesize }} = File.stat(filename)
-
-    stream   = File.stream!(filename, [ :read ], 1)
-    data     = ""
-    max_meta = 128 * 1024
-
-    if filesize > max_meta do
-      meta_drop = filesize - max_meta
-      data      = stream |> Enum.take(meta_drop) |> Enum.join()
-      stream    = stream |> Enum.drop(meta_drop)
-    end
-
-    split_data(data, stream)
+    filename
+      |> File.read!
+      |> :binary.split(@metadata_marker)
+      |> maybe_succeed()
   end
 
-  # <<171>> == first char of @metadata_marker
-  # 14      == byte_size of @metadata_marker
-  defp split_data(data, stream) do
-    size_old  = byte_size(data)
-    data      = data <> (stream |> Enum.take_while( &(&1 != <<171>>) ) |> Enum.join())
-    size_read = byte_size(data) - size_old
-
-    if 0 < size_read do
-      stream = stream |> Enum.drop(size_read)
-    end
-
-    maybe_marker = stream |> Enum.take(14) |> Enum.join()
-    stream       = stream |> Enum.drop(14)
-
-    cond do
-      maybe_marker == @metadata_marker -> { data, Enum.join(stream) }
-      size_read == 0                   -> { :error, :no_metadata }
-      true -> split_data(data <> maybe_marker, stream)
-    end
-  end
+  defp maybe_succeed([ _data ]),      do: { :error, :no_metadata }
+  defp maybe_succeed([ data, meta ]), do: { data, meta }
 end
