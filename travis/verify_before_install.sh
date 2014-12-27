a7dd55bbae53554bfaca4ad30943fd46d4eb1597
Marc Neudert <marc.neudert@gmail.com>
2014-12-27 18:00:35 +0100

    downloads databases for result verification

--- travis/verify_before_install.sh
+++ travis/verify_before_install.sh
@@ -9,3 +9,13 @@ wget -q "https://github.com/elixir-lang/elixir/releases/download/v${ELIXIR}/Prec
 unzip -qq Precompiled.zip -d vendor/elixir
 
 export PATH="${PATH}:${TRAVIS_BUILD_DIR}/vendor/elixir/bin"
+
+# fetch geolite2 databases
+mkdir -p data
+
+cd data
+
+wget -q "http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz"
+wget -q "http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz"
+
+gunzip *
