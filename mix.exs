76cd79c3adebef87df421c4da5e1449f704bb96b
Marc Neudert <marc.neudert@gmail.com>
2015-04-23 19:44:16 +0200

    adds CHANGELOG to hex package

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule Geolix.Mixfile do
 
   def package do
     %{ contributors: [ "Marc Neudert" ],
-       files:        [ "LICENSE", "mix.exs", "README.md", "lib" ],
+       files:        [ "CHANGELOG.md", "LICENSE", "mix.exs", "README.md", "lib" ],
        licenses:     [ "Apache 2.0" ],
        links:        %{ "Docs" => @url_docs, "Github" => @url_github }}
   end
