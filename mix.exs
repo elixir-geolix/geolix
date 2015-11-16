9f5503057b0247e8ff7354abf7046ef2646188af
Marc Neudert <marc.neudert@gmail.com>
2015-11-16 19:12:16 +0100

    bumps version to v0.10.0-dev

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Geolix.Mixfile do
       name:          "Geolix",
       description:   "MaxMind GeoIP2 database reader/decoder",
       package:       package,
-      version:       "0.9.0",
+      version:       "0.10.0-dev",
       elixir:        "~> 1.0",
       deps:          deps(Mix.env),
       docs:          docs,
@@ -40,7 +40,7 @@ defmodule Geolix.Mixfile do
   def docs do
     [ extras:     [ "CHANGELOG.md", "README.md" ],
       main:       "extra-readme",
-      source_ref: "v0.9.0",
+      source_ref: "master",
       source_url: @url_github ]
   end
 
