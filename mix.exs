a0022086d8c048d879e548631156505664727d47
Marc Neudert <marc.neudert@gmail.com>
2015-12-26 14:29:16 +0100

    updates dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Geolix.Mixfile do
   def deps(:docs) do
     deps(:prod) ++
       [ { :earmark, "~> 0.1",  optional: true },
-        { :ex_doc,  "~> 0.10", optional: true } ]
+        { :ex_doc,  "~> 0.11", optional: true } ]
   end
 
   def deps(:test) do
@@ -39,7 +39,7 @@ defmodule Geolix.Mixfile do
 
   def docs do
     [ extras:     [ "CHANGELOG.md", "README.md" ],
-      main:       "extra-readme",
+      main:       "readme",
       source_ref: "master",
       source_url: @url_github ]
   end
