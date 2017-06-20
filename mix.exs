e87450ef9af4f0f78d4020a8cbe21a488cc742cd
Marc Neudert <marc.neudert@gmail.com>
2017-06-20 19:12:16 +0200

    updates dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Geolix.Mixfile do
 
   defp deps do
     [ { :ex_doc,      ">= 0.0.0", only: :dev },
-      { :excoveralls, "~> 0.6",   only: :test },
+      { :excoveralls, "~> 0.7",   only: :test },
       { :hackney,     "~> 1.0",   only: :test },
 
       { :poolboy, "~> 1.0" } ]
