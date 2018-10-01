6348978f3f344cd505d1b727830f1e737ed7c950
Marc Neudert <marc.neudert@gmail.com>
2018-10-01 19:55:43 +0200

    improves Fake adapter documentation

--- lib/geolix/adapter/fake.ex
+++ lib/geolix/adapter/fake.ex
@@ -1,29 +1,47 @@
 defmodule Geolix.Adapter.Fake do
   @moduledoc """
   Fake adapter for testing environments.
+
+  ## Usage
+
+  This adapter is intended to be used only with static data you can provide
+  when the adapter is started, i.e. when performing unit tests.
+
+      iex> Geolix.load_database(%{
+      ...>   id: :fake_sample,
+      ...>   adapter: Geolix.Adapter.Fake,
+      ...>   data: %{
+      ...>     {127, 0, 0, 1} => "IPv4",
+      ...>     {0, 0, 0, 0, 0, 0, 0, 1} => "IPv6"
+      ...>   }
+      ...> })
+      :ok
+      iex> Geolix.lookup("127.0.0.1", where: :fake_sample)
+      "IPv4"
+      iex> Geolix.lookup("::1", where: :fake_sample)
+      "IPv6"
+      iex> Geolix.lookup("255.255.255.255", where: :fake_sample)
+      nil
+
+  The lookup is done by exactly matching the IP address tuple received
+  and will return the predefined result as is.
   """
 
   alias Geolix.Adapter.Fake.Storage
 
   @behaviour Geolix.Adapter
 
+  @impl Geolix.Adapter
   def database_workers() do
     import Supervisor.Spec
 
     [worker(Storage, [])]
   end
 
-  @doc """
-  Implementation of `Geolix.Adapter.load_database/1`.
-
-  Requires the parameter `:data` to be a map with all database entries.
-
-  Each database entry is one entry in the map. The key should be an exact
-  IP address in a format returned by `:inet.parse_address/1` while the result
-  can be any term.
-  """
+  @impl Geolix.Adapter
   def load_database(%{data: data, id: id}), do: Storage.set(id, data)
 
+  @impl Geolix.Adapter
   def lookup(ip, opts) do
     case opts[:where] do
       nil -> nil
@@ -31,5 +49,6 @@ defmodule Geolix.Adapter.Fake do
     end
   end
 
+  @impl Geolix.Adapter
   def unload_database(%{id: id}), do: Storage.set(id, nil)
 end
--- test/geolix/adapter/fake_test.exs
+++ test/geolix/adapter/fake_test.exs
@@ -1,30 +1,5 @@
 defmodule Geolix.Adapter.FakeTest do
   use ExUnit.Case, async: true
 
-  alias Geolix.Adapter.Fake
-
-  @base_spec %{id: :fake, adapter: Fake, data: %{}}
-  @test_ip4 {8, 8, 8, 8}
-  @test_ip6 {0, 0, 0, 0, 0, 65535, 2056, 2056}
-
-  test "empty data returns nil" do
-    Geolix.load_database(@base_spec)
-
-    assert nil == Geolix.lookup(@test_ip4, where: :fake)
-    assert nil == Geolix.lookup(@test_ip6, where: :fake)
-  end
-
-  test "pre-filled database lookup" do
-    term = :fake_adapter_data_term
-
-    data =
-      %{}
-      |> Map.put(@test_ip4, term)
-      |> Map.put(@test_ip6, term)
-
-    Geolix.load_database(%{@base_spec | data: data})
-
-    assert term == Geolix.lookup(@test_ip4, where: :fake)
-    assert term == Geolix.lookup(@test_ip6, where: :fake)
-  end
+  doctest Geolix.Adapter.Fake
 end
