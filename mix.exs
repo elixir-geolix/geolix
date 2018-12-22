79c2b6ebedf689c5b72f46bf25debb89f109572f
Marc Neudert <marc.neudert@gmail.com>
2018-12-22 14:14:46 +0100

    updates :mmdb2_decoder

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule Geolix.Mixfile do
       {:excoveralls, "~> 0.10", only: :test},
       {:geolix_testdata, "~> 0.3.0", only: [:bench, :test]},
       {:hackney, "~> 1.0", only: :test},
-      {:mmdb2_decoder, "~> 0.3.0"},
+      {:mmdb2_decoder, "~> 0.4.0"},
       {:poolboy, "~> 1.0"}
     ]
   end
