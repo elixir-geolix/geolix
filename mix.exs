b9640404adcb7128514cccdc80889efd89734134
Marc Neudert <marc.neudert@gmail.com>
2019-02-07 19:03:37 +0100

    adds :dialyxir dependency

--- mix.exs
+++ mix.exs
@@ -43,6 +43,7 @@ defmodule Geolix.Mixfile do
     [
       {:benchee, "~> 0.13.0", only: :bench},
       {:credo, "~> 1.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 1.0.0-rc", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:excoveralls, "~> 0.10", only: :test},
       {:geolix_testdata, "~> 0.3.0", only: [:bench, :test]},
