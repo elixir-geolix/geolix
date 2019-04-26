420f2c663105a55f6b693fe5d4ddf6e359766dab
Marc Neudert <marc.neudert@gmail.com>
2019-04-26 20:14:14 +0200

    improves test process supervision

--- test/geolix/supervisor_test.exs
+++ test/geolix/supervisor_test.exs
@@ -4,7 +4,9 @@ defmodule Geolix.SupervisorTest do
   import ExUnit.CaptureLog
 
   defmodule Initializer do
-    def start_link, do: Agent.start_link(fn -> nil end, name: __MODULE__)
+    use Agent
+
+    def start_link(_), do: Agent.start_link(fn -> nil end, name: __MODULE__)
 
     def call_init, do: call_init(:ok_empty)
     def call_init(result), do: Agent.update(__MODULE__, fn _ -> result end)
@@ -14,7 +16,6 @@ defmodule Geolix.SupervisorTest do
 
   setup_all do
     init = Application.get_env(:geolix, :init)
-    {:ok, _} = Initializer.start_link()
 
     on_exit(fn ->
       :ok = Application.put_env(:geolix, :init, init)
@@ -22,6 +23,8 @@ defmodule Geolix.SupervisorTest do
   end
 
   test "init {mod, fun} called upon supervisor (re-) start" do
+    {:ok, _} = start_supervised(Initializer)
+
     capture_log(fn ->
       Supervisor.stop(Geolix.Supervisor, :normal)
 
@@ -35,6 +38,8 @@ defmodule Geolix.SupervisorTest do
   end
 
   test "init {mod, fun, args} called upon supervisor (re-) start" do
+    {:ok, _} = start_supervised(Initializer)
+
     capture_log(fn ->
       Supervisor.stop(Geolix.Supervisor, :normal)
 
