55e7615c18cf5d35f2ad0c2435a573a6f6d03696
Marc Neudert <marc.neudert@gmail.com>
2019-06-08 14:39:13 +0200

    Use Task.async_stream/3 for parallel lookups

--- lib/geolix/server/worker.ex
+++ lib/geolix/server/worker.ex
@@ -27,14 +27,14 @@ defmodule Geolix.Server.Worker do
   defp lookup_all(_, _, []), do: %{}
 
   defp lookup_all(ip, opts, databases) do
-    # credo:disable-for-lines:7 Credo.Check.Refactor.MapInto
     databases
-    |> Enum.map(fn database ->
-      task_opts = Keyword.put(opts, :where, database)
-
-      {database, Task.async(fn -> lookup_single(ip, task_opts) end)}
-    end)
-    |> Enum.into(%{}, fn {database, task} -> {database, Task.await(task)} end)
+    |> Task.async_stream(
+      fn database ->
+        {database, lookup_single(ip, [{:where, database} | opts])}
+      end,
+      ordered: false
+    )
+    |> Enum.into(%{})
   end
 
   defp lookup_single(ip, opts) do
