45a32c8330a3df4b2ae301dd8d04a340f8ab561d
Marc Neudert <marc.neudert@gmail.com>
2019-10-27 13:21:56 +0100

    Update dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Geolix.MixProject do
       {:credo, "~> 1.0", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0-rc", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.11", only: :test, runtime: false},
+      {:excoveralls, "~> 0.12", only: :test, runtime: false},
       {:poolboy, "~> 1.0"}
     ]
   end
