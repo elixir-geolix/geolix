4fcbb7138af1acedd62a13d53d6a494dcb8c1bf3
Marc Neudert <marc.neudert@gmail.com>
2020-07-01 18:45:40 +0200

    Remove state agents from test adapters

--- test/geolix/database/loader_delayed_test.exs
+++ test/geolix/database/loader_delayed_test.exs
@@ -6,61 +6,26 @@ defmodule Geolix.Database.LoaderDelayedTest do
   defmodule DelayedAdapter do
     @behaviour Geolix.Adapter
 
-    defmodule DelayedStorage do
-      use Agent
-
-      def start_link(_state), do: Agent.start_link(fn -> %{} end, name: __MODULE__)
-      def get(action), do: Agent.get(__MODULE__, &Map.get(&1, action))
-      def set(action, id), do: Agent.update(__MODULE__, &Map.put(&1, action, id))
+    def load_database(%{notify: pid}) do
+      send(pid, :load_database)
+      :delayed
     end
 
-    def database_workers(_database) do
-      [DelayedStorage.child_spec(%{})]
-    end
-
-    def load_database(_) do
-      case DelayedStorage.get(:load_database) do
-        nil ->
-          DelayedStorage.set(:load_database, :initial)
-          :delayed
-
-        :initial ->
-          DelayedStorage.set(:load_database, :final)
-      end
-    end
-
-    def unload_database(%{id: id}), do: DelayedStorage.set(:unload_database, id)
-
-    def lookup(_ip, _opts, _database), do: :ok
+    def lookup(_, _, _), do: nil
   end
 
   test "load/unload delayed" do
     id = :loader_delayed
-
-    db = %{
-      id: id,
-      adapter: __MODULE__.DelayedAdapter
-    }
+    db = %{id: id, adapter: DelayedAdapter, notify: self()}
 
     assert :delayed = Geolix.load_database(db)
 
-    assert :initial = DelayedAdapter.DelayedStorage.get(:load_database)
-    refute DelayedAdapter.DelayedStorage.get(:unload_database)
+    assert_receive :load_database
 
     assert %{id: ^id, state: :delayed} = Loader.get_database(id)
-
     assert id in Loader.registered_databases()
     refute id in Loader.loaded_databases()
 
-    assert :ok = Geolix.load_database(db)
-
-    assert :final = DelayedAdapter.DelayedStorage.get(:load_database)
-    assert %{id: ^id, state: :loaded} = Loader.get_database(id)
-
-    assert id in Loader.registered_databases()
-    assert id in Loader.loaded_databases()
-
     assert :ok = Geolix.unload_database(id)
-    assert ^id = DelayedAdapter.DelayedStorage.get(:unload_database)
   end
 end
--- test/geolix/database/loader_test.exs
+++ test/geolix/database/loader_test.exs
@@ -6,22 +6,17 @@ defmodule Geolix.Database.LoaderTest do
   defmodule LifecycleAdapter do
     @behaviour Geolix.Adapter
 
-    defmodule LifecycleStorage do
-      use Agent
-
-      def start_link(_state), do: Agent.start_link(fn -> %{} end, name: __MODULE__)
-      def get(action), do: Agent.get(__MODULE__, &Map.get(&1, action))
-      def set(action, id), do: Agent.update(__MODULE__, &Map.put(&1, action, id))
+    def load_database(%{notify: pid}) do
+      send(pid, :load_database)
+      :ok
     end
 
-    def database_workers(_database) do
-      [LifecycleStorage.child_spec(%{})]
+    def unload_database(%{notify: pid}) do
+      send(pid, :unload_database)
+      :ok
     end
 
-    def load_database(%{id: id}), do: LifecycleStorage.set(:load_database, id)
-    def unload_database(%{id: id}), do: LifecycleStorage.set(:unload_database, id)
-
-    def lookup(_ip, _opts, _database), do: :ok
+    def lookup(_, _, _), do: nil
   end
 
   test "fetching un-registered database information" do
@@ -44,16 +39,11 @@ defmodule Geolix.Database.LoaderTest do
 
   test "load/unload lifecycle" do
     id = :loader_lifecycle
-
-    db = %{
-      id: id,
-      adapter: __MODULE__.LifecycleAdapter
-    }
+    db = %{id: id, adapter: LifecycleAdapter, notify: self()}
 
     assert :ok = Geolix.load_database(db)
 
-    assert ^id = LifecycleAdapter.LifecycleStorage.get(:load_database)
-    refute LifecycleAdapter.LifecycleStorage.get(:unload_database)
+    assert_receive :load_database
 
     assert %{id: ^id, state: :loaded} = Loader.get_database(id)
 
@@ -61,6 +51,7 @@ defmodule Geolix.Database.LoaderTest do
     assert id in Loader.loaded_databases()
 
     assert :ok = Geolix.unload_database(id)
-    assert ^id = LifecycleAdapter.LifecycleStorage.get(:unload_database)
+
+    assert_receive :unload_database
   end
 end
