78c31c5e249de69afe2f032961bbd6c5d71b8090
Marc Neudert <marc.neudert@gmail.com>
2020-07-21 18:58:13 +0200

    Extend loader lifecycle test with a full reload

--- test/geolix/database/loader_test.exs
+++ test/geolix/database/loader_test.exs
@@ -1,5 +1,5 @@
 defmodule Geolix.Database.LoaderTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
 
   alias Geolix.Database.Loader
 
@@ -19,14 +19,16 @@ defmodule Geolix.Database.LoaderTest do
     def lookup(_, _, _), do: nil
   end
 
-  test "load/unload lifecycle" do
+  test "load/reload/unload lifecycle" do
     id = :loader_lifecycle
     db = %{id: id, adapter: LifecycleAdapter, notify: self()}
 
     refute Loader.get_database(:database_not_loaded)
 
     assert :ok = Geolix.load_database(db)
+    assert :ok = Geolix.reload_databases()
 
+    assert_receive :load_database
     assert_receive :load_database
 
     assert %{id: ^id, state: :loaded} = Loader.get_database(id)
