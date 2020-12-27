e2e4cf3a33e7752e930be68e32af991b720f2529
Marc Neudert <marc.neudert@gmail.com>
2020-12-27 14:22:51 +0100

    Remove obsolete "coveralls.travis" config

--- mix.exs
+++ mix.exs
@@ -16,8 +16,7 @@ defmodule Geolix.MixProject do
       package: package(),
       preferred_cli_env: [
         coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.travis": :test
+        "coveralls.detail": :test
       ],
       test_coverage: [tool: ExCoveralls]
     ]
