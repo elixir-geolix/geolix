8a54c91f491af3f2ddb6cbac775e046795788fd9
Thomas Citharel <github@tcit.fr>
2021-09-22 17:45:39 +0200

    Change license metadata field to a valid SPDX identifier
    Not mandatory, but recommended for hex.pm. https://hex.pm/docs/publish#adding-metadata-to-code-classinlinemixexscode
    
    Can be useful for a project like https://github.com/Cantido/hex_licenses

--- mix.exs
+++ mix.exs
@@ -62,7 +62,7 @@ defmodule Geolix.MixProject do
   defp package do
     %{
       files: ["CHANGELOG.md", "LICENSE", "mix.exs", "README.md", "lib"],
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       links: %{"GitHub" => @url_github}
     }
   end
